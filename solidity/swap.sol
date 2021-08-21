47139fddd77e3f65a41cbf3b2ddc3d56ecaab700
Crypt0-Bear <cubobear@gmail.com>
2021-08-21 12:54:44 -0500

    Update swap.sol

--- solidity/swap.sol
+++ solidity/swap.sol
@@ -1,7 +1,28 @@
 pragma solidity ^0.6.0;
+/*
+__  __
+|  \/  |
+| \  / | ___  _ __   ___ _ __ ___
+| |\/| |/ _ \| '_ \ / _ \ '__/ _ \
+| |  | | (_) | | | |  __/ | | (_) |
+|_|__|_|\___/|_| |_|\___|_|  \___/
+|  ____| | | |
+| |__  | |_| |__   ___ _ __ ___ _   _ _ __ ___
+|  __| | __| '_ \ / _ \ '__/ _ \ | | | '_ ` _ \
+| |____| |_| | | |  __/ | |  __/ |_| | | | | | |
+|______|\__|_| |_|\___|_|  \___|\__,_|_|_|_| |_|
+   /\  | |                (_)       / ____|
+  /  \ | |_ ___  _ __ ___  _  ___  | (_____      ____ _ _ __
+ / /\ \| __/ _ \| '_ ` _ \| |/ __|  \___ \ \ /\ / / _` | '_ \
+/ ____ \ || (_) | | | | | | | (__   ____) \ V  V / (_| | |_) |
+/_/    \_\__\___/|_| |_| |_|_|\___| |_____/ \_/\_/ \__,_| .__/
+                                                       | |
+                                                       |_|
 
+https://github.com/Crypt0-Bear/xmr-eth-swap-playground
+https://twitter.com/Crypt0_Bear
 
-
+*/
 
 import "OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/math/SafeMath.sol";
 import "OpenZeppelin/openzeppelin-contracts@3.0.0/contracts/utils/Counters.sol";
@@ -23,16 +44,19 @@ contract Swap{
 
   using SafeMath for uint256;
   using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
+  Counters.Counter private _swapId;
 
   address public  alice;
   address public  bob;
   bool public locked;
   string public aK;
+  //Eventually need to move this to a struct for knowing what hash function was used etc
   string public hashed_aK;
   uint public lockTime;
   uint public startBlock;
-  uint public deposit;
+  uint public depositBalance;
+  event Receive(uint value);
+  event Withdraw(string _correct_aK);
 
 
 
@@ -47,6 +71,12 @@ contract Swap{
       deposit = msg.value
     }
 
+function () payable {
+  Receive(msg.value);
+  depositBalance += msg.value;
+}
+
+
 function lockContract(string hashed_aK,) public returns (bool){
   bob = msg.sender
   hashed_aK = hashed_aK;
@@ -54,11 +84,22 @@ function lockContract(string hashed_aK,) public returns (bool){
   return true;
 }
 
+//this is interacted by Bob to withdraw his ether from the contract.
+/**
+TODOs:
+Need to make sure that the eth transaction which interacts with this function is
+signing the value of aK when submitted. If not I must also include the signatre of aK as part of the
+input for the function
+
+**/
 function withdraw(string _aK) public returns(bool){
   require (locked == true);
-//todo keep goiing zzzzzzz
-
-
+  require ( sha256(string(_ak)) == hashed_aK);
+  ak = string(_ak);
+  //send eth to the withdrawer
+  bob.send(depositBalance);
+  //emit event and make it easier for alice to track value of the contract
+  emit Withdraw(ak);
 
 
 }
